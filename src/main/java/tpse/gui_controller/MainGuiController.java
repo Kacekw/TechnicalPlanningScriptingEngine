19519f6935f8b3393febdccf1e4db387b545e201
Kacper Witos <kaclaw@gmail.com>
2020-01-31 12:01:06 +0100

    Tabs were taken out from main.fxml to be individual fxml files with its own controllers

--- src/main/java/tpse/gui_controller/MainGuiController.java
+++ src/main/java/tpse/gui_controller/MainGuiController.java
@@ -7,5 +7,18 @@ import tpse.windows_layer_connector.sap.SapConnection;
 
 public class MainGuiController {
 
+    @FXML
+    private Label sapConnectedBottomLeftLabel;
 
+    public void checkVersion() {
+        //TODO remove that part when finished with configuring sap connections details
+        System.out.println("Connecting to SAPGui");
+        SapConnection sapConnection = new SapConnection();
+        ActiveXComponent sapGui = sapConnection.getSapGui();
+        String connectionDetails = String.format("Connected to Sap version %s.%s", sapGui.getProperty("MajorVersion"), sapGui.getProperty("MinorVersion"));
+        sapConnectedBottomLeftLabel.setText(connectionDetails);
+        System.out.println(connectionDetails);
+        sapConnection.disconnect();
+        System.out.println("Disconnected from SAP");
+    }
 }
---
+++ src/main/java/tpse/gui_controller/scheduled_tab/ScheduledTabController.java
@@ -0,0 +1,4 @@
+package tpse.gui_controller.scheduled_tab;
+
+public class ScheduledTabController {
+}
